62f9366029a25e9c715b52e0f7ecf4108e12bdb2
JeongJinUng <jinung.dev@gmail.com>
2023-03-09 23:44:20 +0900

    JPA를 이용한 사용자 목록 조회 - GET HTTP Method

--- src/main/java/com/example/restfulwebservice/user/MemberJpaController.java
+++ src/main/java/com/example/restfulwebservice/user/MemberJpaController.java
@@ -1,8 +1,12 @@
 package com.example.restfulwebservice.user;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +20,18 @@ public class MemberJpaController {
     @GetMapping("/users")
     public List<Member> retrieveAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public EntityModel<Member> retrieveUser(@PathVariable int id) {
+        Optional<Member> member = userRepository.findById(id);
+        if (!member.isPresent()) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+        EntityModel<Member> memberEntityModel = EntityModel.of(member.get());
+        WebMvcLinkBuilder linkto = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).retrieveAllUsers());
+        memberEntityModel.add(linkto.withRel("all-users"));
+        return memberEntityModel;
     }
 
 
