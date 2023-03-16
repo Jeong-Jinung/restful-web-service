831f84e590a8a9e745b06658866a3c56692c266a
JeongJinUng <jinung.dev@gmail.com>
2023-03-16 22:51:36 +0900

    JPA를 이용한 사용자 추가와 삭제 - POST/DELETE HTTP Method

--- src/main/java/com/example/restfulwebservice/user/MemberJpaController.java
+++ src/main/java/com/example/restfulwebservice/user/MemberJpaController.java
@@ -1,14 +1,21 @@
 package com.example.restfulwebservice.user;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/jpa")
@@ -32,6 +39,21 @@ public class MemberJpaController {
         WebMvcLinkBuilder linkto = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).retrieveAllUsers());
         memberEntityModel.add(linkto.withRel("all-users"));
         return memberEntityModel;
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        userRepository.deleteById(id);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<Member> createUser(@Valid @RequestBody Member member) {
+        Member save = userRepository.save(member);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+            .path("/{id}")
+            .buildAndExpand(save.getId())
+            .toUri();
+        return ResponseEntity.created(uri).build();
     }
 
 
